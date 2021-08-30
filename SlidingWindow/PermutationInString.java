5b63116523dcbd6e53a954df878aba86be4ed2e7
Chirag Tagadiya <cr.tagadiya@gmail.com>
2021-08-30 17:37:12 -0400

    LC 567 Permutation in String Array with int as map

---
+++ SlidingWindow/PermutationInString.java
@@ -0,0 +1,72 @@
+/*
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+ 
+
+Constraints:
+
+1 <= s1.length, s2.length <= 104
+s1 and s2 consist of lowercase English letters.
+
+
+
+*/
+
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        
+        
+        
+        if(s1.length() > s2.length()){
+            return false;
+        }
+        
+       int [] s1Map = new int[26];
+        
+        for(int i =0;i<s1.length(); i++) {
+            s1Map[s1.charAt(i) - 'a']++;
+        }
+        
+        for(int i = 0; i <= s2.length() - s1.length();i++) {
+            
+            int [ ]s2Map = new int[26];
+            
+            for(int j=0;j< s1.length();j++) {
+                s2Map[s2.charAt(i+j) - 'a']++;
+            }
+            
+            if(matches(s1Map,s2Map)) {
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
+    
+    
+    private boolean matches(int[] map1, int[] map2) {
+        
+        for(int i=0; i<  26;i++) {
+            if(map1[i] !=map2[i]){
+                return false;
+            }
+        }
+        
+        
+        return true;
+    }
+}
