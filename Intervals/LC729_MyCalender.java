34bcbd7e3bc4d92e565aa934803b261d015bb3b1
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-01-10 08:17:29 -0500

    Update LC729_MyCalender.java

--- Intervals/LC729_MyCalender.java
+++ Intervals/LC729_MyCalender.java
@@ -130,3 +130,46 @@ class MyCalendar {
 
 
 */
+Approach #2: Balanced Tree [Accepted]
+Intuition
+
+If we maintained our events in sorted order, we could check whether an event could be booked in O(\log N)O(logN) time (where NN is the number of events already booked) by binary searching for where the event should be placed. We would also have to insert the event in our sorted structure.
+
+Algorithm
+
+We need a data structure that keeps elements sorted and supports fast insertion. In Java, a TreeMap is the perfect candidate. In Python, we can build our own binary tree structure.
+
+For Java, we will have a TreeMap where the keys are the start of each interval, and the values are the ends of those intervals. When inserting the interval [start, end), we check if there is a conflict on each side with neighboring intervals: we would like calendar.get(prev)) <= start <= end <= next for the booking to be valid (or for prev or next to be null respectively.)
+
+For Python, we will create a binary tree. Each node represents some interval [self.start, self.end) while self.left, self.right represents nodes that are smaller or larger than the current node.
+
+
+
+/*
+
+class MyCalendar {
+    
+    TreeMap<Integer, Integer> sorted_map;
+
+    public MyCalendar() {
+        sorted_map = new TreeMap();
+    }
+    
+    public boolean book(int start, int end) {
+        // greatest key less or equals to start
+        Integer prev = sorted_map.floorKey(start);
+        // smallest key greater or equal to end
+        Integer next = sorted_map.ceilingKey(start);
+        
+        // conflict interval
+        if( (prev == null || sorted_map.get(prev) <= start ) && 
+           (next == null ||   end <= next ) ) {
+                sorted_map.put(start, end);
+                return true;
+           }
+        
+        return false;
+    }
+}
+
+*/
