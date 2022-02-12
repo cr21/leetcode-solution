94fe0812f2c4bf6a734688df68c2ea9a3b3d016b
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-02-11 23:20:04 -0500

    Update LC123BuyAndSellStock3.java

--- Array/LC123BuyAndSellStock3.java
+++ Array/LC123BuyAndSellStock3.java
@@ -33,7 +33,7 @@ Constraints:
 */
 
 class Solution {
-    public int maxProfit(int[] prices) {
+    public int maxProfit2(int[] prices) {
         // find out what you will get if you are doing only one transaction
         // try to add first transaction into buying point for 2nd transaction, so finally
         // 2nd transaction profit will combine overall profit of 2 transaction
@@ -55,5 +55,48 @@ class Solution {
         }
         
         return profit2;
+    }
+    
+    /*
+    
+    First, we denote a sequence of prices as Prices[i], with index starting from 0 to N-1. Then we define two arrays, namely left_profits[i] and right_profits[i].
+
+As suggested by the name, each element in the left_profits[i] array would hold the maximum profits that one can gain from doing one single transaction on the left subsequence of prices from the index zero to i, (i.e. Prices[0], Prices[1], ... Prices[i]). For instance, for the subsequences of [7, 1, 5], the corresponding left_profits[2] would be 4, which is to buy the price of 1 and sell it at the price of 5.
+
+And each element in the right_profits[i] array would hold the maximum profits that one can gain from doing one single transaction on the right subsequence of the prices from the index i up to N-1, (i.e. Prices[i], Prices[i+1], ... Prices[N-1]). For example, for the right subsequence of [3, 6, 4], the corresponding right_profits[3] would be 3, which is to buy at the price of 3 and then sell it at the price of 6.
+
+Now, if we divide the sequence of prices around the element at the index i, into two subsequences, with left subsequences as Prices[0], Prices[1], ... Prices[i] and the right subsequence as Prices[i+1], ... Prices[N-1], then the total maximum profits that we obtain from this division (denoted as max_profits[i]) can be expressed as follows: \text{max\_profits[i]} = \text{left\_profits[i]} + \text{right\_profits[i+1]}max_profits[i]=left_profits[i]+right_profits[i+1]
+    */
+    public int maxProfit(int prices[]) {
+        
+        if(prices == null || prices.length <=1) return 0;
+        
+        
+        int leftMin = prices[0];
+        int rightMax = prices[prices.length-1];
+        
+        int [] leftProfits = new int[prices.length];
+        
+        int [] rightProfits = new int[prices.length+1];
+        
+        for(int i=1;i< prices.length;i++) {
+            leftMin = Math.min(leftMin, prices[i]);
+            leftProfits[i] = Math.max(leftProfits[i-1], prices[i]-leftMin);
+            
+        }
+        
+        for(int i = prices.length-1; i>=0;i-- ) {
+            
+            rightMax = Math.max(rightMax, prices[i]);
+            rightProfits[i] = Math.max(rightProfits[i], rightMax - prices[i]);
+        }
+        
+        
+        int maxProfit = 0;
+        
+        for(int i=0;i<prices.length;i++) {
+            maxProfit = Math.max(maxProfit, leftProfits[i]+rightProfits[i+1]);
+        }
+        return maxProfit;
     }
 }
