ed9171bf2fc3a0044ca4a9d0859e21993f67935e
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-02-13 14:34:09 -0500

    Set matrix zeros constant space

--- Array/LC73_SetMatrixZeroes.java
+++ Array/LC73_SetMatrixZeroes.java
@@ -40,51 +40,79 @@ class Solution {
         }
         
         
-        
-//         for(int i=0;i<m;i++) {
-//             for(int j = 0; j<n;j++) {
-//                 System.out.println(" i "+i + " j "+j + " value " + matrix[i][j] );
-//                 if(matrix[i][j] == 0) {
-                    
-//                     // set first element of rows to some value
-//                     matrix[i][0] = Integer.MAX_VALUE;
-//                     matrix[0][j] = Integer.MAX_VALUE;
-//                 }
-//             }
-//         }
+
         
         
-//          for(int i=0;i<m;i++) {
-//             for(int j = 0; j<n;j++) { 
-//                 System.out.println(" i "+i + " j "+j + " value " + matrix[i][j] );
-//                 if(matrix[i][j] == Integer.MAX_VALUE) {
-                    
-//                     if(i == 0 && j!=0) {
-//                         // set col to zero
-//                         for(int k=0;k<m;k++) {
-//                             matrix[k][j]=0;
-//                         }
-//                     }
-                    
-//                     else if( i!= 0 && j == 0 ) {
-//                         for(int k=0;k<n;k++) {
-//                             matrix[i][k]=0;
-//                         }
-//                     }
-//                     else if(i == 0 && j == 0) {
-//                         for(int k=0;k<m;k++) {
-//                             matrix[k][j]=0;
-//                         }
-//                         for(int k=0;k<n;k++) {
-//                             matrix[i][k]=0;
-//                         }
-//                     }
-                    
-//                 }
-                
-//             }
-//         }
+    }
+}
+
+
+
+
+// Constant space solution
+
+class Solution {
+    
+    public void setZeroes(int[][] matrix) {
         
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        
+        
+        
+        boolean isCol = false;
+        
+        for(int i=0;i<m;i++) {
+            
+            // check for the first row first cell
+            // Since first cell for both first row and first column is the same i.e.        matrix[0][0]
+      // We can use an additional variable for either the first row/column.
+      // For this solution we are using an additional variable for the first column
+      // and using matrix[0][0] for the first row.
+            if(matrix[i][0] == 0) {
+                isCol=true;
+            }
+            
+            for(int j = 1; j<n;j++) {
+                // If an element is zero, we set the first element of the corresponding row and column to 0
+                if(matrix[i][j] == 0) {
+                    
+                    // set first element of rows to some value
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+         // Iterate over the array once again and using the first row and first column, update the elements.
+        for(int i = 1;i<m;i++) {
+            for(int j=1;j<n;j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] =0;
+                }
+            }
+        }
+        
+         // See if the first row needs to be set to zero as well
+   
+        if(matrix[0][0] == 0) {
+            for(int j=0;j<n;j++) {
+                matrix[0][j] = 0;
+            }  
+        }
+        
+        // See if the first column needs to be set to zero as well
+        if(isCol == true){
+            for(int j=0;j<m;j++) {
+                matrix[j][0] = 0;
+            }
+            
+        }
+            
+        
+        
+        
+         
         
         
         
